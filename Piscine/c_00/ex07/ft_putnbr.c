bc7047a199cd122d88f16e8450753e09e6a93067
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-15 20:29:23 +0300

    Update ft_putnbr.c

--- Piscine/c_00/ex07/ft_putnbr.c
+++ Piscine/c_00/ex07/ft_putnbr.c
@@ -12,30 +12,24 @@
 
 #include <unistd.h>
 
-void	ft_putchar(char a)
+void ft_putchar(char c)
 {
-	write(1, &a, 1);
+	write(1, &c, 1);
 }
-
-void	ft_putnbr(int nb)
-{
-	if (nb == -2147483648)
+void ft_putnbr(int nb)
+{	
+	if (nb < 0)
 	{
 		ft_putchar('-');
-		ft_putchar('2');
-		ft_putnbr(147483648);
-	}
-	else if (nb < 0)
-	{
-		ft_putchar('-');
-		nb *= -1;
-		ft_putnbr(nb);
+		ft_putnbr(-nb);
 	}
 	else if (nb > 9)
 	{
 		ft_putnbr(nb / 10);
 		ft_putnbr(nb % 10);
 	}
-	else
+	else if (nb <= 9)
+	{
 		ft_putchar(nb + '0');
+	}
 }
