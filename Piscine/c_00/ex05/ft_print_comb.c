62c42c6d477154b5272fc67633a97f977c87c1b4
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-16 11:23:39 +0300

    Update ft_print_comb.c

--- Piscine/c_00/ex05/ft_print_comb.c
+++ Piscine/c_00/ex05/ft_print_comb.c
@@ -30,7 +30,7 @@ void	ft_print_comb(void)
 				write(1, &a, 1);
 				write(1, &b, 1);
 				write(1, &c, 1);
-				if (a != '7' || b != '8' || c != '9')
+				if (a != '7')
 					write(1, ", ", 2);
 				c++;
 			}
