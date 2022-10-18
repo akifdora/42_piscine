26045caf5a0d250a453354504af9a271ad875062
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-18 23:28:28 +0300

    added description lines describing commands.

--- c_00/ex03/ft_print_numbers.c
+++ c_00/ex03/ft_print_numbers.c
@@ -14,12 +14,12 @@
 
 void	ft_print_numbers(void)
 {
-	int	say;
+	int	say; // say adında bir integer değişken tanımlandı
 
-	say = 48;
-	while (say <= 57)
+	say = 48; // 0 rakamının ascii karşılığı
+	while (say <= 57) // 9 rakamının ascii karşılığı, say değişkeni bu sayıdan küçük veya eşit olduğu müddetçe bu döngüye girilecek
 	{
-		write(1, &say, 1);
-		say++;
+		write(1, &say, 1); // say değişkenini ekrana yazdır
+		say++; // 1 arttır
 	}
 }
