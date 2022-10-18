85db55e893a400e9b5cc10a645001b79074e4981
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-18 23:32:46 +0300

    added description lines describing commands.

--- c_00/ex00/ft_putchar.c
+++ c_00/ex00/ft_putchar.c
@@ -12,7 +12,7 @@
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_putchar(char c) // Fonksiyonumuzda bir char parametresi istiyoruz
 {
 	write(1, &c, 1); // Fonksiyonun istediği char parametresini yazdırır
 }
