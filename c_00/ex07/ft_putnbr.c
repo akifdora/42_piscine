8b985ec9c7b15b60c41f44e32d2f38a0e9e6af09
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-19 00:04:35 +0300

    added description lines describing commands.

--- c_00/ex07/ft_putnbr.c
+++ c_00/ex07/ft_putnbr.c
@@ -12,31 +12,31 @@
 
 #include <unistd.h>
 
-void ft_putchar(char c)
+void ft_putchar(char c) // Fonksiyonumuzda bir char parametresi istiyoruz
 {
-	write(1, &c, 1);
+	write(1, &c, 1); // Ekrana a değişkenini yazdır
 }
-void ft_putnbr(int nb)
+void ft_putnbr(int nb) // Fonksiyonumuzda bir int parametresi istiyoruz
 {	
-    if(nb == -2147483648)
+    if(nb == -2147483648) // eğer nb değişkeni -2147483648 sayısına eşitse
     {
-        ft_putchar('-');
-        ft_putchar('2');
-        ft_putchar('1');
-        write(1,"47483648", 8);
+        ft_putchar('-'); // - değerini char'a çevir ve yazdır
+        ft_putchar('2'); // 2 değerini char'a çevir ve yazdır
+        ft_putchar('1'); // 1 değerini char'a çevir ve yazdır
+        write(1,"47483648", 8); // 1 47483648 char'a çevir ve yazdır çünkü bir int değişkeninin alabileceği minimum değer -2147483648 olabileceğinden fonksiyona bu sayı yazıldığında hata verecekti
     }
-	else if (nb < 0)
+	else if (nb < 0) // eğer nb değişkeni 0'dan küçükse
 	{
-		ft_putchar('-');
-		ft_putnbr(-nb);
+		ft_putchar('-'); // - değerini char'a çevir ve yazdır
+		ft_putnbr(-nb); // -nb değerini yazdır
 	}
-	else if (nb > 9)
+	else if (nb > 9) // eğer nb değişkeni 9'dan büyükse
 	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+		ft_putnbr(nb / 10); // nb değerini 10'a böl ve yazdır
+		ft_putnbr(nb % 10); // nb değerinin 10'a bölümünden kalanı yazdır
 	}
-	else if (nb <= 9)
+	else if (nb <= 9) // eğer nb değişkeni 9'dan küçükse veya eşitse
 	{
-		ft_putchar(nb + '0');
+		ft_putchar(nb + 48); // nb değişkenini int tipinden char'a çevirebilmek için 0 sayısının ascii değeri olan 48 ile toplayarak ascii'ye uygun olacak şekilde yazdırıyoruz
 	}
 }
