8394fc6c3eb0398bc6e3f6acd2cf23cdde932ca8
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-18 23:37:55 +0300

    added description lines describing commands.

--- c_00/ex05/ft_print_comb.c
+++ c_00/ex05/ft_print_comb.c
@@ -14,28 +14,28 @@
 
 void	ft_print_comb(void)
 {
-	int	a;
-	int	b;
-	int	c;
+	int	a; // a adında bir integer değişken tanımlandı
+	int	b; // b adında bir integer değişken tanımlandı
+	int	c; // c adında bir integer değişken tanımlandı
 
-	a = '0';
-	while (a <= '7')
+	a = '0'; // a değişkeni 0'a eşitlendi
+	while (a <= '7') // a değişkeni 7'den küçük veya eşitse döngüye gir
 	{
-		b = a + 1;
-		while (b <= '8')
+		b = a + 1; // b değişkeni a değişkeninin bir fazlası
+		while (b <= '8') // b değişkeni 8'den küçük veya eşitse döngüye gir
 		{
-			c = b + 1;
-			while (c <= '9')
+			c = b + 1; // c değişkeni b değişkeninin bir fazlası
+			while (c <= '9') // c değişkeni 9'den küçük veya eşitse döngüye gir
 			{
-				write(1, &a, 1);
-				write(1, &b, 1);
-				write(1, &c, 1);
-				if (a != '7')
-					write(1, ", ", 2);
-				c++;
+				write(1, &a, 1); // a değişkenini ekrana yazdır
+				write(1, &b, 1); // b değişkenini ekrana yazdır
+				write(1, &c, 1); // c değişkenini ekrana yazdır
+				if (a != '7') // eğer a değişkeni 7'ye eşit değilse
+					write(1, ", ", 2); // ekrana ", " yazdır
+				c++; // c değişkenini 1 arttır
 			}
-			b++;
+			b++; // b değişkenini 1 arttır
 		}
-		a++;
+		a++; // a değişkenini 1 arttır
 	}
 }
