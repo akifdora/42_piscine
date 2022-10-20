e65378b8b49f269bc0a49aa136f0a534ef768938
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-20 13:05:15 +0300

    Add files via upload

---
+++ c_01/ex07/ft_rev_int_tab_main.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab_main.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdora <mdora@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 02:18:52 by mdora             #+#    #+#             */
+/*   Updated: 2022/10/16 02:18:55 by mdora            ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_rev_int_tab(int *tab, int size)
+{
+	int	a;
+	int b;
+	int rev;
+
+	a = 0;
+	b = size - 1;
+	while (a < size / 2)
+	{
+		rev = tab[a];
+		tab[a] = tab[b];
+		tab[b] = rev;
+		a++;
+		b--;
+	}
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	int tab[] = {1,2,3,4,};
+	int size;
+	size = 4;
+	int i;
+	
+	ft_rev_int_tab(tab, size);
+	i = 0;
+	while (i < size)
+	{
+		printf("%d", tab[i]);
+		i++;
+	}
+}
+
+
