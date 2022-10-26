06fa53a875c8c2434c436195f55ee20420ebe05a
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-26 10:09:25 +0300

    Add files via upload

---
+++ c_02/ex05/ft_str_is_uppercase_main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase_main.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdora <mdora@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 22:58:47 by mdora             #+#    #+#             */
+/*   Updated: 2022/10/25 22:58:49 by mdora            ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	main()
+{
+       printf("%d", ft_str_is_uppercase("ABCDEFGH"));
+       printf("\n%d", ft_str_is_uppercase("ABCDEFGHaI"));
+       printf("\n%d", ft_str_is_uppercase("-_134556ABCDEFGaH67"));
+}
