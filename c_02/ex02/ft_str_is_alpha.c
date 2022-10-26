f734209097f291c18027e461cdde148dff8b1b23
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-26 10:06:51 +0300

    Update ft_str_is_alpha.c

--- c_02/ex02/ft_str_is_alpha.c
+++ c_02/ex02/ft_str_is_alpha.c
@@ -17,11 +17,11 @@ int	ft_str_is_alpha(char *str)
 	int	i;
 
 	i = 0;
-	if (str[i] == '\0' )
+	if (str[i] == '\0')
 	{
 		return (1);
 	}
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		if ((str[i] >= 'A' && str [i] <= 'Z')
 			|| (str [i] >= 'a' && str [i] <= 'z'))
