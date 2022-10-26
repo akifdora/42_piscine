0bfc2e91902b792b2f732b3340751f1b816bf803
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-26 10:07:58 +0300

    Add files via upload

---
+++ c_02/ex03/ft_str_is_numeric_main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric_main.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdora <mdora@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 22:48:35 by mdora             #+#    #+#             */
+/*   Updated: 2022/10/25 22:49:27 by mdora            ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 48 && str [i] <= 57))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	main()
+{
+	printf("%d", ft_str_is_numeric("05092000"));
+	printf("\n%d", ft_str_is_numeric("0509akif2000"));
+	printf("\n%d", ft_str_is_numeric(".(akif05dora_?"));
+}
