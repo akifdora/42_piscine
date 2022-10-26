5e0b174623a0e0a379f4de55425b3da90bd6649c
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-26 10:13:22 +0300

    Update ft_strcapitalize_main.c

--- c_02/ex09/ft_strcapitalize_main.c
+++ c_02/ex09/ft_strcapitalize_main.c
@@ -17,7 +17,7 @@ char    *ft_strlowcase(char *str)
     int    i;
 
     i = 0;
-    while (str[i] != '\0')
+    while (str[i])
     {
         if (str[i] >= 'A' && str[i] <= 'Z')
             str[i] += 32;
@@ -34,7 +34,7 @@ char    *ft_strcapitalize(char *str)
     i = 0;
     i1 = 1;
     ft_strlowcase(str);
-    while (str[i] != '\0')
+    while (str[i])
     {
         if (str[i] >= 'a' && str[i] <= 'z')
         {
