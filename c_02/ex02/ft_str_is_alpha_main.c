bd95232fdd5ff7ba24f294ede5529c86acbd2ad8
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-26 10:07:09 +0300

    Add files via upload

---
+++ c_02/ex02/ft_str_is_alpha_main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha_main.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdora <mdora@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/22 21:15:14 by mdora             #+#    #+#             */
+/*   Updated: 2022/10/25 22:47:54 by mdora            ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '\0' )
+	{
+		return (1);
+	}
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'A' && str [i] <= 'Z')
+			|| (str [i] >= 'a' && str [i] <= 'z'))
+			i++;
+		else
+			return (0);
+	}	
+	return (1);
+}
+int	main()
+{
+	printf("%d", ft_str_is_alpha("abcdefghijkl"));
+	printf("\n%d", ft_str_is_alpha("abc1defghijkl"));
+	printf("\n%d", ft_str_is_alpha("-_134556efghij67"));
+}
