0cd4ea03f3bd7ba6b3c66d1e83607465ce7610e4
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-26 10:08:46 +0300

    Add files via upload

---
+++ c_02/ex04/ft_str_is_lowercase_main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase_main.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdora <mdora@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 22:58:08 by mdora             #+#    #+#             */
+/*   Updated: 2022/10/25 22:58:16 by mdora            ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	main()
+{
+	printf("%d", ft_str_is_lowercase("abcdefghijkl"));
+        printf("\n%d", ft_str_is_lowercase("abcAdefghijkl"));
+        printf("\n%d", ft_str_is_lowercase("-_134556efSghij67"));
+}
