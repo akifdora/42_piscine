f8e67464d9245f32d0de6a205dad6f1e0429568f
Akif DORA <32612500+akifdora@users.noreply.github.com>
2022-10-30 14:01:34 +0300

    Update ft_strstr.c

--- c_03/ex04/ft_strstr.c
+++ c_03/ex04/ft_strstr.c
@@ -10,20 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strstr(char *str, char *find)
+char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	if (find[0] == '\0')
+	if (to_find[0] == '\0')
 		return (str);
 	while (str[i])
 	{
 		j = 0;
-		while (str[i + j] == find[j] && str[i])
+		while (str[i + j] == to_find[j] && str[i])
 		{
-			if (find[j + 1] == '\0')
+			if (to_find[j + 1] == '\0')
 				return (&str[i]);
 			j++;
 		}
