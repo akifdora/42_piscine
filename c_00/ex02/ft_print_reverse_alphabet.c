f1d6be517963c299acfb40fe48da18b48b44b109
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-02 17:40:44 +0300

    Update ft_print_reverse_alphabet.c

--- c_00/ex02/ft_print_reverse_alphabet.c
+++ c_00/ex02/ft_print_reverse_alphabet.c
@@ -17,7 +17,7 @@ void	ft_print_reverse_alphabet(void)
 	int	say; // say adında bir integer değişken tanımlandı
 
 	say = 122; // z harfinin ascii karşılığı
-	while (say >= 98) // a harfinin ascii karşılığı, say değişkeni bu sayıdan büyük veya eşit olduğu müddetçe bu döngüye girilecek
+	while (say >= 97) // a harfinin ascii karşılığı, say değişkeni bu sayıdan büyük veya eşit olduğu müddetçe bu döngüye girilecek
 	{
 		write(1, &say, 1); // say değişkenini ekrana yazdır
 		say--; // 1 azalt
