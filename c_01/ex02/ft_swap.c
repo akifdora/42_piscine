95956d5acb4d846633bb35f1407c23df6ee6ab7b
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-04 00:26:52 +0300

    added description lines describing commands.

--- c_01/ex02/ft_swap.c
+++ c_01/ex02/ft_swap.c
@@ -12,12 +12,12 @@
 
 #include <unistd.h>
 
-void	ft_swap(int *a, int *b) // Fonksiyonumuzda 2 adet integer tipinde parametre gönderilmesini istiyoruz
+void	ft_swap(int *a, int *b) // Fonksiyonumuzda 2 adet integer tipinde parametre gönderilmesini istiyoruz.
 {
-	int	tmp; // tmp adında integer tipinde bir değişken oluşturuyoruz
+	int	tmp; // tmp adında integer tipinde bir değişken oluşturuyoruz.
 
-	tmp = *a; // Fonksiyonda gönderilen a parametresinin değerini tmp değişkenine veriyoruz
-	*a = *b; // Fonksiyonda gönderilen b parametresinin değerini a değişkenine veriyoruz
-	*b = tmp; // İlk başta a parametresinden gelen değeri tmp'ye göndermiştik şimdi de tmp değerini b değişkenine veriyoruz
-	// Ve bu sayede fonksiyonda gönderilen a parametresindeki değer ile b parametresindeki değer yer değiştirmiş oluyor
+	tmp = *a; // Fonksiyonda gönderilen a parametresinin değerini tmp değişkenine veriyoruz.
+	*a = *b; // Fonksiyonda gönderilen b parametresinin değerini a değişkenine veriyoruz.
+	*b = tmp; // İlk başta a parametresinden gelen değeri tmp'ye göndermiştik şimdi de tmp değerini b değişkenine veriyoruz.
+	// Ve bu sayede fonksiyonda gönderilen a parametresindeki değer ile b parametresindeki değer yer değiştirmiş oluyor.
 }
