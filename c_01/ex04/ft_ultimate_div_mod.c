77279a07af326a5a36a0a48c05e03bc3b7480c32
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-04 00:30:46 +0300

    added description lines describing commands.

--- c_01/ex04/ft_ultimate_div_mod.c
+++ c_01/ex04/ft_ultimate_div_mod.c
@@ -12,13 +12,13 @@
 
 #include <unistd.h>
 
-void	ft_ultimate_div_mod(int *a, int *b)
+void	ft_ultimate_div_mod(int *a, int *b) // Fonksiyonumuzda 2 adet integer tipinde parametre gönderilmesini istiyoruz.
 {
-	int	div;
-	int	mod;
+	int	div; // div adında integer tipinde bir değişken oluşturuyoruz.
+	int	mod; // mod adında integer tipinde bir değişken oluşturuyoruz.
 
-	div = *a / *b;
-	mod = *a % *b;
-	*a = div;
-	*b = mod;
+	div = *a / *b; // a parametresinin değerinin b parametresinin değerine bölümünden çıkan değeri div değişkenine atıyoruz.
+	mod = *a % *b; // a parametresinin değerinin b parametresinin değerine modundan çıkan değeri div değişkenine atıyoruz.
+	*a = div; // div değişkenindeki değeri a değişkenine atıyoruz.
+	*b = mod; // mod değişkenindeki değeri b değişkenine atıyoruz.
 }
