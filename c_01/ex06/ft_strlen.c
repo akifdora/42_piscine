4ee7c90010e40887f2b4a689bda7a8354ecfb811
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-04 00:38:27 +0300

    added description lines describing commands.

--- c_01/ex06/ft_strlen.c
+++ c_01/ex06/ft_strlen.c
@@ -12,14 +12,14 @@
 
 #include <unistd.h>
 
-int	ft_strlen(char *string)
+int	ft_strlen(char *string) // Fonksiyonumuzda 1 adet char tipinde bir string parametresi gönderilmesini istiyoruz. Char tipindeki bu parametre pointer'lı olduğu için string halini alabiliyor.
 {
-	int	n;
+	int	n; // n adında integer tipinde bir değişken oluşturuyoruz.
 
-	n = 0;
-	while (string[n] != '\0')
+	n = 0; // n'in değerini 0'a eşitliyoruz.
+	while (string[n] != '\0') // Eğer string değişkeninin n değerindeki indeksini boşluğa eşit değilse bir döngüye giriyoruz.
 	{
-		n++;
+		n++; // n değişkeninin değerini 1 arttırıyoruz.
 	}
-	return (n);	
+	return (n); // n değerini döndürüyoruz ve bu sayede ekranda n değeri kaçsa o çıktıyı görüyoruz.
 }
