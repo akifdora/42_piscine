68b7f3e41fd93d0ef4b8e77cedb93ab798fb5a9e
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-05 01:30:12 +0300

    added description lines describing commands.

--- c_01/ex07/ft_rev_int_tab.c
+++ c_01/ex07/ft_rev_int_tab.c
@@ -12,20 +12,21 @@
 
 #include <unistd.h>
 
-void ft_rev_int_tab(int *tab, int size)
+void ft_rev_int_tab(int *tab, int size) // Fonksiyonumuzda 2 adet integer tipinde parametre gönderilmesini istiyoruz.
 {
-	int	a;
-	int b;
-	int rev;
+	int	a; // a adında integer tipinde bir değişken oluşturuyoruz.
+	int b; // b adında integer tipinde bir değişken oluşturuyoruz.
+	int rev; // rev adında integer tipinde bir değişken oluşturuyoruz.
 
-	a = 0;
-	b = size - 1;
-	while (a < size / 2)
+	a = 0; // a değişkeninin değerini 0'a eşitliyoruz.
+	b = size - 1; // b değişkeninin değerini size değişkeninin 1 eksiğine eşitliyoruz.
+	while (a < size / 2) // Eğer a değişkeninin değeri size değişkeninin değerinin 2'ye bölümünden küçükse döngüye giriyoruz.
 	{
-		rev = tab[a];
-		tab[a] = tab[b];
-		tab[b] = rev;
-		a++;
-		b--;
+		rev = tab[a]; // tab parametresinin a değerindeki indeksini rev değişkenine veriyoruz.
+		tab[a] = tab[b]; // tab parametresinin b değerindeki indeksini tab parametresinin a değerindeki indekse veriyoruz.
+		tab[b] = rev; // rev değişkeninin değerini tab parametresinin b değerindeki indekse veriyoruz.
+		a++; // a değişkeninin değerini 1 arttırıyoruz.
+		b--; // b değişkeninin değerini 1 azaltıyoruz.
+		// Bu işlem sonucunda [1,2,3,4] şeklinde sahip olduğumuz bir dizinin son çıktısı (projede istenilen durum ilk giren son çıkar olduğundan [4,3,2,1] olacaktır.
 	}
 }
