4d2911d6d038d0cfd7d90bb742788788f1b52895
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 17:26:33 +0300

    added description lines describing commands.

--- c_03/ex04/ft_strstr.c
+++ c_03/ex04/ft_strstr.c
@@ -10,24 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strstr(char *str, char *to_find)
+char	*ft_strstr(char *str, char *to_find) // Fonksiyonumuzda 2 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
-	int	j;
+	int	i; // i adında integer bir değişken tanımlıyoruz.
+	int	j; // j adında integer bir değişken tanımlıyoruz.
 
-	i = 0;
-	if (to_find[0] == '\0')
-		return (str);
-	while (str[i])
+	i = 0; // i değişkenini 0'a eşitliyoruz.
+	if (to_find[0] == '\0') // to_find değişkeninin sıfırıncı indeksi boşluğa eşitse bu koşula giriyoruz.
+		return (str); // str değişkenini döndürüp ekrana yazdırıyoruz.
+	while (str[i]) // str değişkeninin i değerindeki indeksi varsa bu koşula giriyoruz.
 	{
-		j = 0;
-		while (str[i + j] == to_find[j] && str[i])
+		j = 0; // j değişkenini 0'a eşitliyoruz.
+		while (str[i + j] == to_find[j] && str[i]) // str değişkeninin i ve j değerlerinin toplamındaki indeksi to_find değişkeninin j değerindeki indekse eşitse ve str değişkeninin i değerindeki indeksi varsa bu koşula giriyoruz.
 		{
-			if (to_find[j + 1] == '\0')
-				return (&str[i]);
-			j++;
+			if (to_find[j + 1] == '\0') // to_find değişkenin j değerinin bir fazlasındaki indeksi boşluğa eşitse bu koşula giriyoruz.
+				return (&str[i]); // str değişkeninin i değerindeki indeksinin karakterini döndürüp yazdırıyoruz. Bu adres, daha sonra verilen to_find dizisiyle eşleşen uygun alt diziye döndürmek için kullanılan pointerda saklanır.
+			j++; // j değerini 1 arttırıyoruz.
 		}
-		i++;
+		i++; // i değerini 1 arttırıyoruz.
 	}
-	return (0);
+	return (0); // 0 döndürüp ekrana yazdırıyoruz.
 }
