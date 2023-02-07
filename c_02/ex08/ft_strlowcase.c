16dfbcf02c84831da73de969ce9f1921a80fffdb
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 16:51:04 +0300

    added description lines describing commands.

--- c_02/ex08/ft_strlowcase.c
+++ c_02/ex08/ft_strlowcase.c
@@ -10,18 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strlowcase(char *str)
+char	*ft_strlowcase(char *str) // Fonksiyonumuzda 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
+	int	i; // i adında integer bir değişken tanımlıyoruz.
 
-	i = 0;
-	while (str[i])
+	i = 0; // i değişkenini 0'a eşitliyoruz.
+	while (str[i]) // str değişkeninin i değerindeki indeksi varsa döngüye giriyoruz.
 	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
+		if (str[i] >= 'A' && str[i] <= 'Z') // str değişkeninin i değerindeki indeksi büyük A harfinden büyükse/eşitse ve büyük Z harfinden küçükse/eşitse bu koşula giriyoruz.
 		{
-			str[i] += 32;
+			str[i] += 32; // str değişkeninin i değerinindeki indeksinde bulunan alfabetik karakteri küçültmek için ASCII tablosunda 32 değer arttırıyoruz.
 		}
-		i++;
+		i++; // i değerini 1 arttırıyoruz.
 	}
-	return (str);
+	return (str); // str değişkeninin değerini döndürüp ekrana yazdırıyoruz.
 }
