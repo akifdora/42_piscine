f623e5db6563c4d584aeebf6527cb884e01da4e7
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 11:26:44 +0300

    added description lines describing commands.

--- c_02/ex02/ft_str_is_alpha.c
+++ c_02/ex02/ft_str_is_alpha.c
@@ -10,22 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_alpha(char *str)
+int	ft_str_is_alpha(char *str) // Fonksiyonumuzda 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
+	int	i; // i adında integer bir değişken tanımlıyoruz.
 
-	i = 0;
-	if (str[i] == '\0')
+	i = 0; // i değerini 0'a eşitliyoruz.
+	if (str[i] == '\0') // Eğer str değişkeninin i değerindeki indeksi boşluğa eşitse bu koşula giriyoruz.
 	{
-		return (1);
+		return (1); // 1 döndürüp ekrana yazdırıyoruz.
 	}
-	while (str[i])
+	while (str[i]) // str değişkeninin i değerindeki indeksi varsa döngüye giriyoruz.
 	{
-		if ((str[i] >= 'A' && str [i] <= 'Z')
-			|| (str [i] >= 'a' && str [i] <= 'z'))
-			i++;
-		else
-			return (0);
+		if ((str[i] >= 'A' && str[i] <= 'Z') // str değişkeninin i değerindeki indeksi büyük A harfinden büyükse/eşitse ve büyük Z harfinden küçükse/eşitse ya da küçük a harfinden büyükse/eşitse ve küçük z harfinden küçükse/eşitse bu koşula giriyoruz.
+			|| (str[i] >= 'a' && str[i] <= 'z'))
+			i++; // i değerini 1 arttırıyoruz.
+		else // if koşulu tutmuyorsa buraya giriyoruz.
+			return (0); // 0 döndürüp ekrana yazdırıyoruz.
 	}	
-	return (1);
+	return (1); // 1 döndürüp ekrana yazdırıyoruz.
 }
