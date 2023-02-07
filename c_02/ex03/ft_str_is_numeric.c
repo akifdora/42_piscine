d392c0f2c062ea687be20e294a1843255326c7f5
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 11:30:46 +0300

    added description lines describing commands.

--- c_02/ex03/ft_str_is_numeric.c
+++ c_02/ex03/ft_str_is_numeric.c
@@ -10,18 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_numeric(char *str)
+int	ft_str_is_numeric(char *str) // Fonksiyonumuzda 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
+	int	i; // i adında integer bir değişken tanımlıyoruz.
 
-	i = 0;
-	while (str[i])
+	i = 0; // i değerini 0'a eşitliyoruz.
+	while (str[i]) // str değişkeninin i değerindeki indeksi varsa bu döngüye giriyoruz.
 	{
-		if (!(str[i] >= 48 && str [i] <= 57))
+		if (!(str[i] >= 48 && str [i] <= 57)) // str değişkeninin i indeksindeki değeri ASCII tablosundaki 48 değerinden büyük/eşit değilse ve 57 değerinden küçük/eşit değilse döngüye giriyoruz.
 		{
-			return (0);
+			return (0); // 0 döndürüp ekrana yazdırıyoruz.
 		}
-		i++;
+		i++; // i değerini 1 arttırıyoruz.
 	}
-	return (1);
+	return (1); // 1 döndürüp ekrana yazdırıyoruz.
 }
