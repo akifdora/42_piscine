0a05e225085a052b01c9a5959d700f0b7c8ad10d
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 10:41:49 +0300

    added description lines describing commands.

--- c_02/ex00/ft_strcpy.c
+++ c_02/ex00/ft_strcpy.c
@@ -10,16 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dest, char *src)
+char	*ft_strcpy(char *dest, char *src) // Fonksiyonumuzda 2 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
+	int	i; // i adında integer tipinde bir değişken oluşturuyoruz.
 
-	i = 0;
-	while (src[i])
+	i = 0; // i'nin değerini 0'a eşitliyoruz.
+	while (src[i]) // Eğer src değişkeninin i değerindeki indeksi varsa bir döngüye giriyoruz.
 	{
-		dest[i] = src[i];
-		i++;
+		dest[i] = src[i]; // src değişkeninin i değerindeki indeksini dest değişkeninin i değerindeki indeksine gönderiyoruz.
+		i++; // i değişkenini 1 arttırıyoruz.
 	}
-	dest[i] = '\0';
-	return (dest);
+	dest[i] = '\0'; // Kopyalama işlemi bitip, döngüden çıktıktan sonra dest değişkeninin i değerindeki indeksini boşluğa eşitliyoruz.
+	return (dest); // dest değerini döndürüp ekrana yazdırıyoruz.
 }
