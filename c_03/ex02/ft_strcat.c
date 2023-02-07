a3900b7da88560caffc3e8c8b8208d52f0ef8280
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 17:12:14 +0300

    added description lines describing commands.

--- c_03/ex02/ft_strcat.c
+++ c_03/ex02/ft_strcat.c
@@ -10,20 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcat(char *dest, char *src)
+char	*ft_strcat(char *dest, char *src) // Fonksiyonumuzda 2 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	x;
-	int	y;
+	int	x; // x adında integer bir değişken tanımlıyoruz.
+	int	y; // y adında integer bir değişken tanımlıyoruz.
 
-	x = 0;
-	while (dest[x])
-		x++;
-	y = 0;
-	while (src[y])
+	x = 0; // x değişkenini 0'a eşitliyoruz.
+	while (dest[x]) // dest değişkeninin x değerindeki indeksi varsa bu koşula giriyoruz.
+		x++; // x değerini 1 arttırıyoruz.
+	y = 0; // y değişkenini 0'a eşitliyoruz.
+	while (src[y]) // src değişkeninin y değerindeki indeksi varsa bu koşula giriyoruz.
 	{
-		dest[x + y] = src[y];
-		y++;
+		dest[x + y] = src[y]; // src değişkenin y değerindeki indeksini dest değişkeninin x ve y değerinin toplamındaki indeksine eşitliyoruz.
+		y++; // y değerini bir arttırıyoruz.
 	}
-	dest[x + y] = '\0';
-	return (dest);
+	dest[x + y] = '\0'; // dest değişkeninin x ve y değerinin toplamındaki indeksini boşluğa eşitliyoruz.
+	return (dest); // dest değişkeninin değerini döndürüp ekrana yazdırıyoruz.
 }
