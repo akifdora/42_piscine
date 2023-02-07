4fb61df9a8de4f0a0d34fa124642194a0323aa22
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 16:49:41 +0300

    added description lines describing commands.

--- c_02/ex07/ft_strupcase.c
+++ c_02/ex07/ft_strupcase.c
@@ -10,18 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strupcase(char *str)
+char	*ft_strupcase(char *str) // Fonksiyonumuzda 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
+	int	i; // i adında integer bir değişken tanımlıyoruz.
 
-	i = 0;
-	while (str[i])
+	i = 0; // i değişkenini 0'a eşitliyoruz.
+	while (str[i]) // str değişkeninin i değerindeki indeksi varsa döngüye giriyoruz.
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z') // str değişkeninin i değerindeki indeksi küçük a harfinden büyükse/eşitse ve küçük z harfinden küçükse/eşitse bu koşula giriyoruz.
 		{
-			str[i] -= 32;
+			str[i] -= 32; // str değişkeninin i değerinindeki indeksinde bulunan alfabetik karakteri büyütmek için ASCII tablosunda 32 değer eksiltiyoruz.
 		}
-		i++;
+		i++; // i değerini 1 arttırıyoruz.
 	}
-	return (str);
+	return (str); // str değişkeninin değerini döndürüp ekrana yazdırıyoruz.
 }
