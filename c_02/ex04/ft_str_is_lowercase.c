41fd48959445968c62a0e2905302fba817ee692c
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 11:33:29 +0300

    added description lines describing commands.

--- c_02/ex04/ft_str_is_lowercase.c
+++ c_02/ex04/ft_str_is_lowercase.c
@@ -10,18 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_lowercase(char *str)
+int	ft_str_is_lowercase(char *str) // Fonksiyonumuzda 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
+	int	i; // i adında integer bir değişken oluşturuyoruz.
 
-	i = 0;
-	while (str[i])
+	i = 0; // i değişkenini 0'a eşitliyoruz.
+	while (str[i]) // str değişkeninin i değerindeki indeksi varsa döngüye giriyoruz.
 	{
-		if (!(str[i] >= 'a' && str[i] <= 'z'))
+		if (!(str[i] >= 'a' && str[i] <= 'z')) // str değişkeninin i değerindeki indeksi küçük a harfinden büyük/eşit değilse ve küçük z harfinden küçük/eşit değilse bu koşula giriyoruz.
 		{
-			return (0);
+			return (0); // 0 döndürüp ekrana yazdırıyoruz.
 		}
-		i++;
+		i++; // i değerini 1 arttırıyoruz.
 	}
-	return (1);
+	return (1); // 1 döndürüp ekrana yazdırıyoruz.
 }
