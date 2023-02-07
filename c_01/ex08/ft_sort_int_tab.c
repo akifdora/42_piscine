db2e313d994cff895ff858962d56e69bcafa34ba
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 23:43:21 +0300

    added description lines describing commands.

--- c_01/ex08/ft_sort_int_tab.c
+++ c_01/ex08/ft_sort_int_tab.c
@@ -1,21 +1,33 @@
-void	ft_sort_int_tab(int *tab, int size)
-{
-	int	i;
-	int	swap;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdora <mdora@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 02:15:43 by mdora             #+#    #+#             */
+/*   Updated: 2022/10/16 02:19:26 by mdora            ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-	while (size >= 0)
+void	ft_sort_int_tab(int *tab, int size) // Fonksiyonumuzda 2 adet integer tipinde parametre gönderilmesini istiyoruz.
+{
+	int	i; // i adında integer tipinde bir değişken oluşturuyoruz.
+	int	swap; // swap adında integer tipinde bir değişken oluşturuyoruz.
+
+	while (size >= 0) // size değişkeninin değeri 0'dan büyükse/eşitse bu döngüye giriyoruz.
 	{
-		i = 0;
-		while (i < size - 1)
+		i = 0; // i değerini 0'a eşitliyoruz.
+		while (i < size - 1) // i değişkeninin değeri size değişkeninin değerinin 1 eksiğinden küçükse bu döngüye giriyoruz.
 		{
-			if (tab[i] > tab[i + 1])
+			if (tab[i] > tab[i + 1]) // tab değişkeninin i değerindeki indeksi tab değişkeninin i değerindeki indeksinin 1 fazlasından büyükse bu koşula giriyoruz.
 			{
-				swap = tab [i];
-				tab[i] = tab [i + 1];
-				tab [i + 1] = swap;
+				swap = tab[i]; // tab değişkeninin i değerindeki indeksi swap değişkenine eşitliyoruz.
+				tab[i] = tab[i + 1]; // tab değişkeninin i değerindeki indeksinin 1 fazlasını tab değişkenin i değerindeki indeksine eşitliyoruz.
+				tab[i + 1] = swap; // swap değişkeninin değerini tab değişkeninin i değerindeki indeksinin 1 fazlasına eşitliyoruz.
 			}
-			i++;
+			i++; // i değerini 1 arttırıyoruz.
 		}
-		size--;
+		size--; // size değerini 1 azaltıyoruz.
 	}
 }
