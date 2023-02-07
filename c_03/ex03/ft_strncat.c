976485a891f18c7a5aaeeb7fbedd067fcb04cde2
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 17:15:07 +0300

    added description lines describing commands.

--- c_03/ex03/ft_strncat.c
+++ c_03/ex03/ft_strncat.c
@@ -10,20 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strncat(char *dest, char *src, unsigned int nb)
+char	*ft_strncat(char *dest, char *src, unsigned int nb) // Fonksiyonumuzda 2 adet char ve 1 adet unsigned integer tipinde parametre gönderilmesini istiyoruz.
 {
-	unsigned int	x;
-	unsigned int	y;
+	unsigned int	x; // x adında unsigned integer tipinde bir değişken tanımlıyoruz.
+	unsigned int	y; // y adında unsigned integer tipinde bir değişken tanımlıyoruz.
 
-	x = 0;
-	y = 0;
-	while (dest[x])
-	x++;
-	while (y < nb && src[y])
+	x = 0; // x değişkenini 0'a eşitliyoruz.
+	y = 0; // y değişkenini 0'a eşitliyoruz.
+	while (dest[x]) // dest değişkeninin x indeksindeki değeri varsa bu koşula giriyoruz.
+	x++; // x değerini 1 arttırıyoruz.
+	while (y < nb && src[y]) // y değişkeninin değeri nb değişkeninin değerinden küçükse ve src değişkeninin y değerindeki indeksi varsa bu koşula giriyoruz.
 	{
-		dest[x + y] = src[y];
-		y++;
+		dest[x + y] = src[y]; // src değişkenin y değerindeki indeksini dest değişkeninin x ve y değerinin toplamındaki indeksine eşitliyoruz.
+		y++; // y değerini 1 arttırıyoruz.
 	}
-	dest[x + y] = '\0';
-	return (dest);
+	dest[x + y] = '\0'; // dest değişkeninin x ve y değerinin toplamındaki indeksini boşluğa eşitliyoruz.
+	return (dest); // dest değişkeninin değerini döndürüp ekrana yazdırıyoruz.
 }
