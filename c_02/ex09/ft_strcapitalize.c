c67edcbfa9af79ea5b8ae4241292ee7a41a9cbef
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 17:00:19 +0300

    added description lines describing commands.

--- c_02/ex09/ft_strcapitalize.c
+++ c_02/ex09/ft_strcapitalize.c
@@ -10,41 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strlowcase(char *str)
+char	*ft_strlowcase(char *str) // Fonksiyonumuzda 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
+	int	i; // i adında integer bir değişken tanımlıyoruz.
 
-	i = 0;
-	while (str[i])
+	i = 0; // i değişkenini 0'a eşitliyoruz.
+	while (str[i]) // str değişkeninin i değerindeki indeksi varsa döngüye giriyoruz.
 	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;
-		i++;
+		if (str[i] >= 'A' && str[i] <= 'Z') // str değişkeninin i değerindeki indeksi büyük A harfinden büyükse/eşitse ve büyük Z harfinden küçükse/eşitse bu koşula giriyoruz.
+		{
+			str[i] += 32; // str değişkeninin i değerinindeki indeksinde bulunan alfabetik karakteri küçültmek için ASCII tablosunda 32 değer arttırıyoruz.
+		}
+		i++; // i değerini 1 arttırıyoruz.
 	}
-	return (str);
+	return (str); // str değişkeninin değerini döndürüp ekrana yazdırıyoruz.
 }
 
-char	*ft_strcapitalize(char *str)
+char	*ft_strcapitalize(char *str) // Fonksiyonumuzda 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int		i;
-	int		i1;
+	int		i; // i adında integer bir değişken tanımlıyoruz.
+	int		i1; // i1 adında integer bir değişken tanımlıyoruz.
 
-	i = 0;
-	i1 = 1;
-	ft_strlowcase(str);
-	while (str[i])
+	i = 0; // i değişkenini 0'a eşitliyoruz.
+	i1 = 1; // i1 değişkenini 1'e eşitliyoruz. Bu değişkeni oluşturmamızın sebebi stringte alfabetik karakter dışında herhangi bir karaktere gelip gelmediğimizi anlamak, altta bulunan kodda zaten göreceksiniz, i1 değeri 1'e eşit olduğunda bulunduğumuz harf büyüyecek ve sonrasında o değeri 0'a eşitleyeceğiz. Eğer ki bize gelen stringte boşluk algılarsak i1 değerini tekrar 1'e eşitleyeceğiz ve bu sayede boşluk sonrasındaki kelimenin ilk harfinde i1 değerimiz 1'e eşit olduğundan o harfi de büyüteceğiz.
+	ft_strlowcase(str); // Öncelikle gelen str değerini üstte bulunan fonksiyona gönderip bütün alfabetik karakterlerini küçültüyoruz, çünkü bu exte bizden istenen şey gelen string değerinin sadece baş harflerini büyütmek.
+	while (str[i]) // str değişkeninin i değerindeki indeksi varsa döngüye giriyoruz.
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z') // str değişkeninin i değerindeki indeksi küçük a harfinden büyükse/eşitse ve küçük z harfinden küçükse/eşitse bu koşula giriyoruz.
 		{
-			if (i1 == 1)
-				str[i] -= 32;
-			    i1 = 0;
+			if (i1 == 1) // i1 değişkeni 1'e eşitse bu koşula giriyoruz.
+				str[i] -= 32; // str değişkenin i değerindeki indeksindeki harfi büyütüyoruz.
+			    i1 = 0; // i1 değerini 0'a eşitliyoruz.
 		}
-		else if (str[i] >= '0' && str[i] <= '9')
-			i1 = 0;
-		else
-			i1 = 1;
-		i++;
+		else if (str[i] >= '0' && str[i] <= '9') // str değişkeninin i değerindeki indeksi 0'dan büyükse/eşitse ve 9'dan küçükse/eşitse bu koşula giriyoruz.
+			i1 = 0; // i1 değerini 0'a eşitliyoruz.
+		else // Üstte bulunan koşullar sağlanmıyorsa bu koşula giriyoruz.
+			i1 = 1; // i1 değerini 1'e eşitliyoruz.
+		i++; // i değerini 1 arttırıyoruz.
 	}
-	return (str);
+	return (str); // str değişkeninin değerini döndürüp ekrana yazdırıyoruz.
 }
