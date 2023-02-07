ac94c354286ce615490dd1d505b0cdbd620dfa1f
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 17:08:44 +0300

    added description lines describing commands.

--- c_03/ex01/ft_strncmp.c
+++ c_03/ex01/ft_strncmp.c
@@ -10,14 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(char *s1, char *s2, unsigned int n) // Fonksiyonumuzda 2 adet char ve 1 adet unsigned integer tipinde parametre gönderilmesini istiyoruz.
 {
-	unsigned int	i;
+	unsigned int	i; // i adında unsigned integer tipinde bir değişken tanımlıyoruz.
 
-	if (n == 0)
-		return (0);
-	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
-		i++;
-	return (s1[i] - s2[i]);
+	if (n == 0) // n değeri 0'a eşitse bu koşula giriyoruz.
+		return (0); // 0 döndürüp ekrana yazdırıyoruz.
+	i = 0; // i değişkenini 0'a eşitliyoruz.
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1) // s1 değişkeninin i değerindeki indeksi varsa ve s2 değişkeninin i değerindeki indeksi varsa ve s1 değişkeninin i değerindeki indeksi s2 değişkeninin i değerindeki indeksine eşitse ve i değeri n değerinin bir eksiğinden büyükse bu koşula giriyoruz.
+		i++; // i değerini 1 arttırıyoruz.
+	return (s1[i] - s2[i]); // s1 değişkeninin i değerindeki indeksinden s2 değişkeninin i değerindeki indeksinin ASCII değerlerinin karşılıklarını çıkartıp döndürüyoruz ve ekrana yazdırıyoruz.
 }
