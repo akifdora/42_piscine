2488a52ad0c81d076b79314c964d8227b6dcb684
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 23:09:22 +0300

    added description lines describing commands.

--- c_01/ex04/ft_ultimate_div_mod.c
+++ c_01/ex04/ft_ultimate_div_mod.c
@@ -18,7 +18,7 @@ void	ft_ultimate_div_mod(int *a, int *b) // Fonksiyonumuzda 2 adet integer tipin
 	int	mod; // mod adında integer tipinde bir değişken oluşturuyoruz.
 
 	div = *a / *b; // a parametresinin değerinin b parametresinin değerine bölümünden çıkan değeri div değişkenine atıyoruz.
-	mod = *a % *b; // a parametresinin değerinin b parametresinin değerine modundan çıkan değeri div değişkenine atıyoruz.
+	mod = *a % *b; // a parametresinin değerinin b parametresinin değerine modundan çıkan değeri mod değişkenine atıyoruz.
 	*a = div; // div değişkenindeki değeri a değişkenine atıyoruz.
 	*b = mod; // mod değişkenindeki değeri b değişkenine atıyoruz.
 }
