9cf4743827445104af6d9ea3fc19cc78ddf18859
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-07 11:20:03 +0300

    added description lines describing commands.

--- c_02/ex01/ft_strncpy.c
+++ c_02/ex01/ft_strncpy.c
@@ -10,20 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+char	*ft_strncpy(char *dest, char *src, unsigned int n) // Fonksiyonumuzda 2 adet char tipinde ve 1 adet unsigned integer tipinde parametre gönderilmesini istiyoruz.
 {
-	unsigned int	i;
+	unsigned int	i; // i adında unsigned integer tipinde bir değişken oluşturuyoruz.
 
-	i = 0;
-	while (src[i] && i < n)
+	i = 0; // i'nin değerini 0'a eşitliyoruz.
+	while (src[i] && i < n) // src değişkeninin i değerindeki indeksi varsa ve i'nin değeri n değerinden küçükse döngüye giriyoruz.
 	{
-		dest[i] = src[i];
-		i++;
+		dest[i] = src[i]; // src değişkeninin i değerindeki indeksini dest değişkeninin i değerindeki indeksine gönderiyoruz.
+		i++; // i'nin değerini 1 arttırıyoruz.
 	}
-	while (i < n)
+	while (i < n) // i değişkeninin değeri n değişkeninin değerinden küçükse döngüye giriyoruz.
 	{
-		dest[i] = '\0';
-		i++;
+		dest[i] = '\0'; // dest değişkeninin i değerindeki indeksini boşluğa eşitliyoruz.
+		i++; // i'nin değerini 1 arttırıyoruz.
 	}
-	return (dest);
+	return (dest); // dest değerini döndürüp ekrana yazdırıyoruz.
 }
