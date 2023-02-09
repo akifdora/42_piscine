eecceaa8442d975f039c85fa525b976f0c045e89
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-09 13:20:30 +0300

    added description lines describing commands.

--- c_04/ex01/ft_putstr.c
+++ c_04/ex01/ft_putstr.c
@@ -12,14 +12,14 @@
 
 #include <unistd.h>
 
-void	ft_putstr(char *str)
+void	ft_putstr(char *str) // Fonksiyonumuzda 1 adet char tipinde bir str parametresi gönderilmesini istiyoruz. Char tipindeki bu parametre pointer'lı olduğu için string halini alabiliyor.
 {
-	int	i;
+	int	n; // n adında integer tipinde bir değişken oluşturuyoruz.
 
-	i = 0;
-	while (str[i])
+	n = 0; // n'in değerini 0'a eşitliyoruz.
+	while (str[n] != '\0') // Eğer str değişkeninin n değerindeki indeksini boşluğa eşit değilse bir döngüye giriyoruz.
 	{
-		write (1, &str[i], 1);
-		i++;
-	}
+		write (1, &str[n], 1); // str değişkeninin n değerindeki indeksini ekrana yazdırıyoruz.
+		n++; // n değişkeninin değerini 1 arttırıyoruz.
+	}	
 }
