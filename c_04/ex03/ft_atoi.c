0477add2fabcd1e3b8bb8333e251f3d9f61af704
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-09 13:38:23 +0300

    added description lines describing commands.

--- c_04/ex03/ft_atoi.c
+++ c_04/ex03/ft_atoi.c
@@ -10,27 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(char *str)
+int	ft_atoi(char *str) // Fonksiyonumuzda 1 adet char tipinde parametre gönderilmesini istiyoruz.
 {
-	int	i;
-	int	s;
-	int	res;
+	int	i; // i adında integer tipinde bir değişken tanımlıyoruz.
+	int	s; // s adında integer tipinde bir değişken tanımlıyoruz.
+	int	res; // res adında integer tipinde bir değişken tanımlıyoruz.
 
-	i = 0;
-	s = 1;
-	res = 0;
-	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
-		i++;
-	while (str[i] == '+' || str[i] == '-')
+	i = 0; // i değişkenini 0'a eşitliyoruz.
+	s = 1; // s değişkenini 1'e eşitliyoruz.
+	res = 0; // res değişkenini 0'a eşitliyoruz.
+	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ') // str değişkeninin i değerindeki indeksi TAB'dan büyükse/eşitse ve RETURN'den küçükse/eşitse ya da boşluğa eşitse bu döngüye giriyoruz.
+		i++; // i'nin değerini 1 arttırıyoruz.
+	while (str[i] == '+' || str[i] == '-') // str değişkeninin i değerindeki indeksi +'ya ya da -'ye eşitse bu döngüye giriyoruz.
 	{
-		if (str[i] == '-')
-			s = s * -1;
-		i++;
+		if (str[i] == '-') // str değişkeninin i değerindeki indeksi -'ye eşitse bu koşula giriyoruz.
+			s = s * -1; // s değerinin -1 ile çarpımından çıkan sonucu s değerine eşitliyoruz. Bunu yapmamızın sebebi gelen değeri pozitife döndürmek istememizden kaynaklı.
+		i++; // i'nin değerini 1 arttırıyoruz.
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (str[i] >= '0' && str[i] <= '9') // str değişkeninin i değerindeki indeksi 0'dan büyükse/eşitse ve 9'dan küçükse/eşitse bu döngüye giriyoruz.
 	{
-		res = (str[i] - 48) + (res * 10);
-		i++;
+		res = (str[i] - 48) + (res * 10); // str değişkeninin i değerini 48'den çıkartıyoruz, res değişkeninin değerini 10 ile çarpıyoruz çıkan değerleri toplayıp res değerine eşitliyoruz. Bu işlemin yapılmasının sebebi stringte gelen sayının ASCII değerini algılayıp sayısal karşılığını bulmak.
+		i++; // i'nin değerini 1 arttırıyoruz.
 	}
-	return (res * s);
+	return (res * s); // res değişkeninin değerini s değişkeninin değeri ile çarpıp döndürüp ekrana yazdırıyoruz.
 }
